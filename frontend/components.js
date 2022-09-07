84b806496c044dfc93599d7b4cd8ab7a6ecca9d8
jinwook567 <jinwook567@we2d.com>
2022-09-07 18:32:50 +0900

    컴포넌트에 이벤트 할당 방법 공부중

--- frontend/components.js
+++ frontend/components.js
@@ -1,7 +1,27 @@
-const BookList = () => {};
-
-const Book = () => {};
-
-const createElement = (str) => {
-  const $temp = document.createElement("div");
+const BookList = (bookData) => {
+  const content = `
+    <div>${bookData.map(({ image, name }) => Book({ image, name }).innerHTML)}</div>
+  `;
+  return createElement(content);
 };
+
+const Book = ({ image, name }) => {
+  const content = `<div>
+  <div><h2>${name}</h2></div>
+    <div><image src=${image} alt=${name} width=180/></div>
+  </div>
+  `;
+  const $element = createElement(content);
+  $element.addEventListener("click", () => {
+    history.pushState({}, null, `/detail?name=${name}`);
+  });
+  return $element;
+};
+
+const createElement = (content) => {
+  const $temp = document.createElement("div");
+  $temp.innerHTML = content;
+  return $temp;
+};
+
+export { Book, BookList };
--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,29 @@
+import { Book, BookList } from "./components.js";
+
+class SampleBook {
+  constructor() {
+    this.image = "https://image.yes24.com/goods/105655620/XL";
+    this.name = "월가를 위한 퀀트투자 바이블";
+  }
+}
+
+const book = new SampleBook();
+
+// BookList([book, book])
 const routes = [
-  { path: "/", component: "<div>main</div>", exact: true },
+  { path: "/", component: BookList([book, book, book]), exact: true },
   { path: "/detail", component: "<div>book data</div>", exact: false },
 ];
 
 const $app = document.getElementById("app");
 
 const render = (path) => {
+  console.log("render run");
   const component =
     routes.find((v) => (v.exact ? v.path === path : v.path.includes(path)))?.component ||
     "<div>Not Found</div>";
 
+  console.log(component);
   $app.replaceChildren(component);
 };
 
